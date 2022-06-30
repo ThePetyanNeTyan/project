a3aa87a05e83706b5e2babc28eb47657090c8a88
Petr <example.gmail.com>
2022-06-30 11:08:07 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,43 @@
+//"use strict";
+
+var a = 5;
+console.log(a);
+
+var leftBorderWidth = 1;
+
+const pi = 3.14;
+
+
+
+console.log(4/0);
+console.log("sdsd" * 9);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+console.log(persone["name"]);
+
+let arr = ["plum.png", "orange.jpg", "apple.bmp"];
+
+console.log(arr[0]);
+
+//alert("Hello World!");
+/*let answer = +prompt("Сосать будешь?", "18");
+console.log(answer);
+console.log(typeof(answer));
+*/
+
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" == 2);
+console.log(2 === 2);
